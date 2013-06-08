bb55d0aec35942d133fd9e1495f79ef76ba57527
yty <yutianyu@gmail.com>
2013-06-08 14:43:13 +0800

    Update ofxSprite.h

--- ofxSprite.h
+++ ofxSprite.h
@@ -4,24 +4,43 @@ class ofxSprite{
 
 public:
 
-void setShader(ofShader shader);
+ofxSprite();
+~ofxSprite();
+
+
+void update();
+void draw();
+
+///Transform
 
 void setPosition(ofvec2f pos);
 void setPosition(float x,float y);
 ofvec2f getPosition();
 
-void setAngle(float angle);
-float getAngle();
+void setRotate(float angle);
+float getRotate();
 
 void setScale(ofvec2f scale);
 void setScale(float x,float y);
 ofvec2f getScale();
 
-void setColor(ofColor color);
+///Render
+
+void setTexture(ofTexture* tex) //设置sprite将绘制的纹理
+void setShader(ofShader* shader); //设置sprite将使用的shader
+bool isShader();  //是否使用了shader
+
+void setColor(ofColor color); //设置sprite将使用的color
 ofColor getColor();
+
+private:
+ ofTexture* tex;
+ bool isShader; 
 
 }
 
-///以后可能会更新的内容,根据ofxSprite派生出slicedSprite,tileSprite
-
+///以后可能会更新的内容
+//1根据ofxSprite派生出slicedSprite,tileSprite
+//2添加Near方法
+//3添加Rotate的其他轴
 
