08b3890cf3937f5291bda13fdfd2a85d177309fc
yty <yutianyu@gmail.com>
2013-07-02 11:34:40 +0800

    Update ofxObject.h

--- ofxObject.h
+++ ofxObject.h
@@ -16,14 +16,18 @@ public:
 	ofxObject():
 		size(1.0f),
 		angle(0.0f)
-	{	}
+	{}
   
-	virtual ~ofxObject()	{	}
-	virtual void setShader(ofShader &sd) {shader = sd;}
+	virtual ~ofxObject(){}
+	
+	///Transform
+
 	virtual void setPosition(const ofVec2f &pos) {position = pos;}
+
 	virtual void setRotate(float a) {angle = a;}	//[0 - 360]
 	virtual void setScale(float s) {size = s;}		//(0 - ...)	
 	virtual void setColor(ofColor &c) {color = c;}	//[0 - 255]
-
+	
+	virtual void setShader(ofShader &sd) {shader = sd;}
 	virtual void draw() = 0;
 };
